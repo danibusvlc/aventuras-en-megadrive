ba43f307abacbd69c91f74e2f1f6611706e4ba66
danibusvlc <50983806+danibusvlc@users.noreply.github.com>
2019-09-14 16:18:00 +0200

    Add files via upload

---
+++ leccion 08-shadow & higlight (2)/res/fondos.h
@@ -0,0 +1,7 @@
+#ifndef _RES_FONDOS_H_
+#define _RES_FONDOS_H_
+
+extern const Image fondo1;
+extern const Image fondo2;
+
+#endif // _RES_FONDOS_H_
---
+++ leccion 08-shadow & higlight (2)/res/sprite.res
@@ -0,0 +1,2 @@
+SPRITE sonic_sprite "sprite/sonic.png"  6 6 FAST 0
+SPRITE circulo_sprite "sprite/circulo.png" 5 5 FAST 0
