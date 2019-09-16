094b1321b81fe660eef00657dfb694f14b4dd66f
danibusvlc <50983806+danibusvlc@users.noreply.github.com>
2019-09-16 13:46:36 +0200

    Add files via upload

---
+++ leccion 08-shadow & higlight (3) SF2/res/fondos.res
@@ -0,0 +1,4 @@
+IMAGE fondo1 "fondos/escenario RYU fondo A.png" BEST
+IMAGE fondo2 "fondos/escenario RYU fondo B.png" BEST
+
+
---
+++ leccion 08-shadow & higlight (3) SF2/res/sprite.h
@@ -0,0 +1,8 @@
+#ifndef _RES_SPRITE_H_
+#define _RES_SPRITE_H_
+
+extern const SpriteDefinition mi_sprite_ryu;
+extern const SpriteDefinition mi_sprite_sombra;
+extern const SpriteDefinition mi_sprite_hadoken;
+
+#endif // _RES_SPRITE_H_
